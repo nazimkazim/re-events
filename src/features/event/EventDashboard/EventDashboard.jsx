7d4fd3a66c9ae8c48875ab4e8895f8406f1df6b0
Your Name <nazik_111@mail.ru>
2019-01-20 11:24:11 +0500

    add create event prototype

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 const eventsDashboard = [
   {
@@ -55,15 +56,54 @@ const eventsDashboard = [
 ];
 
 class EventDashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: eventsDashboard,
+      isOpen: false
+    };
+  }
+
+  handleFormOpen = () => {
+    this.setState({
+      isOpen: true
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      isOpen: false
+    });
+  };
+
+  handleCreateEvent = newEvent => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = 'assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    });
+  };
   render() {
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={eventsDashboard} />
+          <EventList events={this.state.events} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button positive content="Create Event" />
-          <EventForm />
+          <Button
+            onClick={this.handleFormOpen}
+            positive
+            content="Create Event"
+          />
+          {this.state.isOpen && (
+            <EventForm
+              createEvent={this.handleCreateEvent}
+              handleCancel={this.handleCancel}
+            />
+          )}
         </Grid.Column>
       </Grid>
     );
