edf8ecf0ee8c5e1f2d7212966464c7f85c63eb6e
Your Name <nazik_111@mail.ru>
2019-01-22 11:43:48 +0500

    add stateless components, home page, routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import { Route, Switch } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import EventForm from '../../features/event/EventForm/EventForm';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import HomePage from '../../features/home/HomePage';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <React.Fragment>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route exact path="/events" component={EventDashboard} />
+                  <Route
+                    exact
+                    path="/event/:id"
+                    component={EventDetailedPage}
+                  />
+                  <Route exact path="/people" component={PeopleDashboard} />
+                  <Route
+                    exact
+                    path="/profile/:id"
+                    component={UserDetailedPage}
+                  />
+                  <Route exact path="/settings" component={SettingsDashboard} />
+                  <Route exact path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </React.Fragment>
+          )}
+        />
       </React.Fragment>
     );
   }
