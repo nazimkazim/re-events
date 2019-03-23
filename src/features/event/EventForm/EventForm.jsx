f2e87d40cc9e75a9dcb91fa2a680fd953f740ddb
Your Name <nazik_111@mail.ru>
2019-03-23 11:33:48 +0600

    add moving events into firestore

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import Script from 'react-load-script';
-import cuid from 'cuid';
+import { withFirestore } from 'react-redux-firebase';
 import {
   composeValidators,
   combineValidators,
@@ -12,31 +12,30 @@ import {
 } from 'revalidate';
 import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import { Segment, Form, Button, Grid, Header } from 'semantic-ui-react';
-import { createEvent, updateEvent } from '../eventActions';
+import { createEvent, updateEvent, cancelToggle } from '../eventActions';
 import TextInput from '../../../app/common/form/TextInput';
 import TextArea from '../../../app/common/form/TextArea';
 import SelectInput from '../../../app/common/form/SelectInput';
 import DateInput from '../../../app/common/form/DateInput';
 import PlaceInput from '../../../app/common/form/PlaceInput';
-import moment from 'moment';
 
-const mapState = (state, ownProps) => {
-  const eventId = ownProps.match.params.id;
-
+const mapState = state => {
   let event = {};
 
-  if (eventId && state.events.length > 0) {
-    event = state.events.filter(event => event.id === eventId)[0];
+  if (state.firestore.ordered.events && state.firestore.ordered.events[0]) {
+    event = state.firestore.ordered.events[0];
   }
 
   return {
-    initialValues: event
+    initialValues: event,
+    event
   };
 };
 
 const actions = {
   createEvent,
-  updateEvent
+  updateEvent,
+  cancelToggle
 };
 
 const category = [
@@ -69,6 +68,11 @@ class EventForm extends Component {
     scriptLoaded: false
   };
 
+  async componentDidMount() {
+    const { firestore, match } = this.props;
+    await firestore.setListener(`events/${match.params.id}`);
+  }
+
   handleCitySelect = selectedCity => {
     geocodeByAddress(selectedCity)
       .then(results => getLatLng(results[0]))
@@ -96,29 +100,30 @@ class EventForm extends Component {
   };
 
   onFormSubmit = values => {
-    values.date = moment(values.date).format();
     values.venueLatLng = this.state.venueLatLng;
     if (this.props.initialValues.id) {
+      if (Object.keys(values.venueLatLng).length === 0) {
+        values.venueLatLng = this.props.event.venueLatLng;
+      }
       this.props.updateEvent(values);
       this.props.history.goBack();
     } else {
-      const newEvent = {
-        ...values,
-        id: cuid(),
-        hostPhotoURL: '/assets/user.png',
-        hostedBy: 'Bob'
-      };
-      this.props.createEvent(newEvent);
+      this.props.createEvent(values);
       this.props.history.push('/events');
     }
   };
+
+  async componentWillUnmount() {
+    const { firestore, match } = this.props;
+    await firestore.unsetListener(`events/${match.params.id}`);
+  }
 
   handleScriptLoaded = () => {
     this.setState({ scriptLoaded: true });
   };
 
   render() {
-    const { invalid, submitting, pristine } = this.props;
+    const { invalid, submitting, pristine, event, cancelToggle } = this.props;
     return (
       <Grid>
         <Script
@@ -192,6 +197,15 @@ class EventForm extends Component {
               <Button onClick={this.props.history.goBack} type="button">
                 Cancel
               </Button>
+              <Button
+                onClick={() => {
+                  cancelToggle(!event.cancelled, event.id);
+                }}
+                type="button"
+                color={event.cancelled ? 'green' : 'red'}
+                floated="right"
+                content={event.cancelled ? 'Reactivate event' : 'Cancel event'}
+              />
             </Form>
           </Segment>
         </Grid.Column>
@@ -199,11 +213,13 @@ class EventForm extends Component {
     );
   }
 }
-export default connect(
-  mapState,
-  actions
-)(
-  reduxForm({ form: 'eventForm', enableReinitialize: true, validate })(
-    EventForm
+export default withFirestore(
+  connect(
+    mapState,
+    actions
+  )(
+    reduxForm({ form: 'eventForm', enableReinitialize: true, validate })(
+      EventForm
+    )
   )
 );
--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -1,18 +1,13 @@
 import { toastr } from 'react-redux-toastr';
-
-import {
-  CREATE_EVENT,
-  DELETE_EVENT,
-  UPDATE_EVENT,
-  FETCH_EVENTS
-} from './eventConstants';
+import { DELETE_EVENT, FETCH_EVENTS } from './eventConstants';
 import {
   asyncActionStart,
   asyncActionFinish,
   asyncActionError
 } from '../async/asyncActions';
-
 import { fetchSampleData } from '../../app/data/mockAPI';
+import { createNewEvent } from '../../app/common/util/helpers';
+import moment from 'moment';
 
 export const fetchEvents = event => {
   return {
@@ -22,13 +17,18 @@ export const fetchEvents = event => {
 };
 
 export const createEvent = event => {
-  return async dispatch => {
+  return async (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    const user = firestore.auth().currentUser;
+    const photoURL = getState().firebase.profile.photoURL;
+    let newEvent = createNewEvent(user, photoURL, event);
     try {
-      dispatch({
-        type: CREATE_EVENT,
-        payload: {
-          event
-        }
+      let createdEvent = await firestore.add(`events`, newEvent);
+      await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
+        eventID: createdEvent.id,
+        userUid: user.uid,
+        eventDate: event.date,
+        host: true
       });
       toastr.success('Success', 'Event has been created');
     } catch (error) {
@@ -38,19 +38,39 @@ export const createEvent = event => {
 };
 
 export const updateEvent = event => {
-  return async dispatch => {
+  return async (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    if (event.date !== getState().firestore.ordered.events[0].date) {
+      event.date = moment(event.date).toDate();
+    }
     try {
-      dispatch({
-        type: UPDATE_EVENT,
-        payload: {
-          event
-        }
-      });
-    toastr.success('Success', 'Event has been updated');
+      await firestore.update(`events/${event.id}`, event);
+      toastr.success('Success', 'Event has been updated');
     } catch (error) {
       toastr.error('Opps', 'Something went wrong');
     }
   };
+};
+
+export const cancelToggle = (cancelled, eventId) => async (
+  dispatch,
+  getState,
+  { getFirestore }
+) => {
+  const firestore = getFirestore();
+  const message = cancelled
+    ? 'Are you sure you want to cancel the event'
+    : 'This will reactivate the event - are you sure';
+  try {
+    toastr.confirm(message, {
+      onOk: () =>
+        firestore.update(`events/${eventId}`, {
+          cancelled: cancelled
+        })
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const deleteEvent = eventID => {
